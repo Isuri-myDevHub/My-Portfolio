80f560ad58f01b0d16e5135055f6f2389c91e549
isuri <isuriwgunaratne@gmail.com>
2023-12-26 18:29:38 +0530

    Initial Commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hi, I am Isuri</h1>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <h1 className='text-white'>Hi, I am Isuri</h1>
     </main>
   )
 }
